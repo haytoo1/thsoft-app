d222a2fe5432568bfc01415e27cb5cd058ee90c3
haytoo <hayto@foxmail.com>
2018-07-23 23:19:59 +0800

    加入PHP数据结构

---
+++ dev/dev1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hong Tu <hayto@foxmail.com>
+ * Date: 2018/7/23
+ * Time: 21:37
+ */
+
+$map = new \Ds\Map();
+$map->put("a", '我是a');
+$map->remove("a");
+$map->remove("a");
