94d57511cfd2e96525e09fbd609d49b145449f58
haytoo <466594257@qq.com>
2018-07-26 09:45:33 +0800

    做了一堆奇怪的工作

--- dev/dev3.php
+++ dev/dev3.php
@@ -13,11 +13,10 @@ $s = microtime(true);
 
 $n= 1000000;
 
-//$map = new \Ds\Map();
-$map = new \Ds\Set();
+$map = new \Ds\Map();
 //$map = [];
 for ($i=0; $i<$n; $i++){
-    $map->add($i);
+    $map->put($i, $i);
 //    $map[$i] = $i;
 }
 for ($i=0; $i<$n-1; $i++){
